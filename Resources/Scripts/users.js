165b0109807659c3ce1c86cd00ec49373b082506
omegadone <36422240+omegadone@users.noreply.github.com>
2025-12-17 09:30:02 -0500

    fixed error on user logon

--- Resources/Scripts/users.js
+++ Resources/Scripts/users.js
@@ -4,7 +4,7 @@ let userDatabase = [];
 // 1. Fetch the user data on load
 async function loadUserData() {
     try {
-        const response = await fetch('users.json');
+        const response = await fetch('user.json');
         const data = await response.json();
         userDatabase = data.users;
         console.log("User database loaded.");
